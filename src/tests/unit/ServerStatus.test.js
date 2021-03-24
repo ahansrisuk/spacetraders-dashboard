1524bfb89f0c71f10668b33c2235c29e8d2776f9
Allen Hansrisuk <allen.hansrisuk@jhuapl.edu>
2021-03-24 17:29:20 -0400

    add axios

--- src/tests/unit/ServerStatus.test.js
+++ src/tests/unit/ServerStatus.test.js
@@ -1,8 +1,18 @@
 import { render, screen } from '@testing-library/react';
 import ServerStatus from '../../components/ServerStatus';
+import axios from 'axios';
 
-test('displays appropriate server status', () => {
+jest.mock('axios');
+
+test('displays online server status', () => {
   render(<ServerStatus />);
-  const status = screen.getByText(/Server Status/i);
+  axios.get.mockImplementation((request) => {
+    if (request.endsWith('status')) {
+      return Promise.resolve({
+        status: 'spacetraders is currently online and available to play',
+      });
+    }
+  });
+  const status = screen.getByText(/Server Status/);
   expect(status).toBeInTheDocument();
 });
